6c0c752d3d951ab8db04e288eff36db2ce9972bf
Kydem <kydemartino1999@gmail.com>
2022-11-22 08:13:02 -0700

    Small edits to formating

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Leaflet API Project (Maps)
 
-// The boilerplate map options and a blank marker
+// The boilerplate map options
 let map = L.map('map', {
     center: [32.2540, -110.9742],
     zoom: 12
 });
+
+// A place holder for hotel markers
+let mark;
 
 //Pre-built Travel Options
 let options = {
@@ -12,9 +15,6 @@ let options = {
     prague: [50.073658, 14.418540],
     guant: [20.0117, -75.1216]
 }
-
-// A place holder for hotel markers
-let mark;
 
 // The map that is displayed, currently using Open Street Map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,6 +28,7 @@ trip.addEventListener("click", () => {
     let i = Math.ceil(Math.random() * 10)
 
     if (i <= 4) {
+        //Flies you to to a new location, and adds a mark where a hotel is
         map.flyTo(new L.LatLng(options.anchorage[0], options.anchorage[1]), 12, {duration: 2});
         mark = L.marker([61.217381, -149.863129]).addTo(map)
     } else if (i > 4 && i <= 8) {
